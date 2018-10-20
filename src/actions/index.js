dd2aef66e62659034bcaa3c276215a718c5a4d5d
이정우 <ijeong-u@ijeong-uui-iMac.local>
2018-10-21 03:11:14 +0900

    [62] Props 렌더링 헬퍼와 매핑

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const API_KEY = 'a2bf025a66228c2f6d70eb022b553ce4';
-const ROOT_URL = `https://samples.openweathermap.org/data/2.5/weather?appid=${API_KEY}`;
+const ROOT_URL = `https://api.openweathermap.org/data/2.5/forecast?appid=${API_KEY}`;
 
 // 액션 생성자와 리듀서 사이의 액션 타입을 계속 일정하게 유지하기 위한 문자
 export const FETCH_WEATHER = 'FETCH_WEATHER';
---
+++ src/containers/weather_list.js
@@ -0,0 +1,43 @@
+import React, { Component } from 'react';
+import { connect } from 'react-redux'
+
+class WeatherList extends Component {
+
+  renderWeather(cityData) {
+
+    const name = cityData.city.name;
+
+    return (
+        <tr key={name}>
+          <td>{name}</td>
+        </tr>
+    )
+  }
+
+  render() {
+    return (
+      <table className="table table-hover">
+        <thead>
+          <tr>
+            <th>City</th>
+            <th>Temperature</th>
+            <th>Pressure</th>
+            <th>Humidity</th>
+          </tr>
+        </thead>
+        <tbody>
+          {this.props.weather.map(this.renderWeather)}
+        </tbody>
+      </table>
+    );
+  }
+}
+
+
+// gloal Application State의 상태를 가져온다.
+function mapStateToProps ({ weather }) {
+  // const weather = state.weather
+  return { weather }; // { weather } === { weather: weather }
+}
+
+export default connect(mapStateToProps)(WeatherList);
