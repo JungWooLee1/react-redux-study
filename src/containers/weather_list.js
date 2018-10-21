58e948c13a143c86b32f701edfeb62482c2c6dcf
이정우 <ijeong-u@ijeong-uui-iMac.local>
2018-10-22 00:16:16 +0900

    [65,66] 차트 설정 및 단위 라벨링

--- src/containers/weather_list.js
+++ src/containers/weather_list.js
@@ -1,15 +1,23 @@
 import React, { Component } from 'react';
-import { connect } from 'react-redux'
+import { connect } from 'react-redux';
+import Chart from'../components/chart'
 
 class WeatherList extends Component {
 
   renderWeather(cityData) {
 
     const name = cityData.city.name;
+    const temps = cityData.list.map(weather => weather.main.temp);
+    const pressures = cityData.list.map(weather => weather.main.pressure);
+    const humidities = cityData.list.map(weather => weather.main.humidity);
 
     return (
         <tr key={name}>
           <td>{name}</td>
+          <td><Chart data={temps} color="orange" units ="K" /></td>
+          <td><Chart data={pressures} color="green" units ="pHa" /></td>
+          <td><Chart data={humidities} color="black" units ="%" /></td>
+
         </tr>
     )
   }
@@ -20,9 +28,9 @@ class WeatherList extends Component {
         <thead>
           <tr>
             <th>City</th>
-            <th>Temperature</th>
-            <th>Pressure</th>
-            <th>Humidity</th>
+            <th>온도 (K)</th>
+            <th>기압 (pHa)</th>
+            <th>습도 (%)</th>
           </tr>
         </thead>
         <tbody>
